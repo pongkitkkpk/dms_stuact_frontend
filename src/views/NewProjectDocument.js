866e08d081532e26464aa36d7d2a6dc4d83435fc
pongkitkkpk <pongkitkim@gmail.com>
2024-04-08 17:03:06 +0700

    listA work la

--- src/views/NewProjectDocument.js
+++ src/views/NewProjectDocument.js
@@ -9,11 +9,11 @@ import CSD_budget from './Compo_ProjectDoc/CreateDetailP/CSD_budget';
 
 function NewProjectDocument() {
   const [id_projects, setIdProjects] = useState('');
-  const [cSD_Detail, setCSD_Detail] = useState(true);//fix edit
-  const [cSD_Detail2, setCSD_Detail2] = useState(false);//fix edit
+  const [cSD_Detail, setCSD_Detail] = useState(true);
+  const [cSD_Detail2, setCSD_Detail2] = useState(false);
   const [cSD_person, setCSD_Person] = useState(false);
-  const [cSD_locationtime, setCSD_LocationTime] = useState(true);
-  const [cSD_timestep, setCSD_timestep] = useState(false);//fix edit
+  const [cSD_locationtime, setCSD_LocationTime] = useState(false);
+  const [cSD_timestep, setCSD_timestep] = useState(false);
   const [cSD_budget, setCSD_budget] = useState(false);
   const containerRef = useRef(null);
 
