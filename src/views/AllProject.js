2bf96bd79af62014945210ab987d5fe957942ae5
Nwpatt <nawapat_link@hotmail.com>
2024-04-18 17:26:35 +0700

    update status and css / link

--- src/views/AllProject.js
+++ src/views/AllProject.js
@@ -11,6 +11,7 @@ import {
   Form,
 } from "react-bootstrap";
 import Admin from "layouts/Admin";
+import Swal from "sweetalert2";
 
 function AllProject() {
   const storedUserData = sessionStorage.getItem("user");
@@ -19,9 +20,9 @@ function AllProject() {
   const studentuser = storedUser.username;
   const strcodebooksomeoutyear = storedUser.codebooksomeoutyear;
   const strcodebooksome = storedUser.codebooksome;
-  useEffect(()=>{
-    console.log(storedUser.account_type)
-  },[storedUser])
+  useEffect(() => {
+    console.log(storedUser.account_type);
+  }, [storedUser]);
 
   const [projectList, setProjectList] = useState([]);
   const [id_student, setIDStudent] = useState(studentuser);
@@ -35,25 +36,32 @@ function AllProject() {
   const filterProjectList = projectList.filter((project) => {
     const yearlyString = project.yearly ? project.yearly.toString() : ""; // Convert yearly to string, handle null case
     return (
-      (project.id_student &&project.id_student.toLowerCase().includes(searchQuery.toLowerCase())) ||
-      (project.project_name &&project.project_name
+      (project.id_student &&
+        project.id_student.toLowerCase().includes(searchQuery.toLowerCase())) ||
+      (project.project_name &&
+        project.project_name
           .toLowerCase()
           .includes(searchQuery.toLowerCase())) ||
-      (project.campus &&project.campus.toLowerCase().includes(searchQuery.toLowerCase())) ||
-      (project.clubName &&project.clubName.toLowerCase().includes(searchQuery.toLowerCase())) ||
-      (project.codeclub &&project.codeclub.toLowerCase().includes(searchQuery.toLowerCase())) ||
-      (project.project_phase &&project.project_phase.toLowerCase().includes(searchQuery.toLowerCase())) ||
-      (project.responsible_agency &&project.responsible_agency
+      (project.campus &&
+        project.campus.toLowerCase().includes(searchQuery.toLowerCase())) ||
+      (project.clubName &&
+        project.clubName.toLowerCase().includes(searchQuery.toLowerCase())) ||
+      (project.codeclub &&
+        project.codeclub.toLowerCase().includes(searchQuery.toLowerCase())) ||
+      (project.project_phase &&
+        project.project_phase
+          .toLowerCase()
+          .includes(searchQuery.toLowerCase())) ||
+      (project.responsible_agency &&
+        project.responsible_agency
           .toLowerCase()
           .includes(searchQuery.toLowerCase())) ||
       (yearlyString && yearlyString.includes(searchQuery.toLowerCase())) // Include yearly in the search
     );
   });
   const getProjects = () => {
-    if (storedUser.account_type == 'admin') {
-      Axios.get(
-        `http://localhost:3001/admin/allprojects`
-      ).then((response) => {
+    if (storedUser.account_type == "admin") {
+      Axios.get(`http://localhost:3001/admin/allprojects`).then((response) => {
         setProjectList(response.data);
       });
     } else {
@@ -64,12 +72,10 @@ function AllProject() {
       });
     }
   };
-  
 
   useEffect(() => {
     getProjects();
   }, []);
-
 
   const formatDate = (timestamp) => {
     const date = new Date(timestamp);
@@ -83,53 +89,68 @@ function AllProject() {
   };
 
   const handleDeleteProject = (id_project, project_name) => {
-    const confirmDelete = window.confirm(`คุณต้องการลบ ${project_name}`);
-
-    if (confirmDelete) {
-      Axios.delete(`http://localhost:3001/student/deleteProject/${id_project}`)
-        .then((response) => {
-          if (response.status === 200) {
-            // Filter out the deleted project from the project list
-            const updatedProjectList = projectList.filter(
-              (project) => project.id !== id_project
-            );
-            setProjectList(updatedProjectList);
-
-            // Show alert after successful deletion
-            alert("Project deleted successfully!");
-          } else {
-            console.error("Failed to delete project");
-          }
-        })
-        .catch((error) => {
-          console.error("Error deleting project:", error);
-        });
-    } else {
-      // User canceled deletion, do nothing
-    }
+    // const confirmDelete = window.confirm();
+    Swal.fire({
+      className: "title",
+      title: `คุณต้องการลบ ${project_name} ใช่หรือไม่?`,
+      text: "การบันทึกข้อมูลจะไม่สามารถยกเลิกได้",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "ยืนยัน",
+      cancelButtonText: "ยกเลิก",
+      reverseButtons: true,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Axios.delete(
+          `http://localhost:3001/student/deleteProject/${id_project}`
+        )
+          .then((response) => {
+            if (response.status === 200) {
+              // Filter out the deleted project from the project list
+              const updatedProjectList = projectList.filter(
+                (project) => project.id !== id_project
+              );
+              setProjectList(updatedProjectList);
+            } else {
+              console.error("Failed to delete project");
+            }
+          })
+          .catch((error) => {
+            console.error("Error deleting project:", error);
+          });
+      }
+    });
   };
 
   const stepTitles = [
     " ร่างคำขออนุมัติ",
     " ดำเนินการขออนุมัติ",
+    " รออนุมัติโครงการ",
     " โครงการอนุมัติ",
+    " รอเงินโครงการอนุมัติ",
     " เงินโครงการอนุมัติ",
+    " รอสรุปผลโครงการ",
     " ร่างสรุปผลโครงการ",
     " ดำเนินการสรุปผล",
-    " ปิดโครงการ"
+    " ปิดโครงการ",
   ];
   const stepColors = {
-    " ร่างคำขออนุมัติ": "#7AC6FF",
-    " ดำเนินการขออนุมัติ": "#8E4E41",
-    " โครงการอนุมัติ": "#44C827",
-    " เงินโครงการอนุมัติ": "#39934F",
-    " ร่างสรุปผลโครงการ": "#0E9E93",
-    " ดำเนินการสรุปผล": "#0E4D9E",
-    " ปิดโครงการ": "#5C7AD6"
+    " ร่างคำขออนุมัติ": "#ff0000",
+    " ดำเนินการขออนุมัติ": "#ff8000",
+    " รออนุมัติโครงการ": "#ff8000",
+    " โครงการอนุมัติ": "#fff000",
+    " รอเงินโครงการอนุมัติ": "#00ff00",
+    " เงินโครงการอนุมัติ": "#00ff00",
+    " รอสรุปผลโครงการ": "#00ffff",
+    " ร่างสรุปผลโครงการ": "#0000ff",
+    " ดำเนินการสรุปผล": "#0000ff",
+    " ปิดโครงการ": "#8000ff",
   };
+
   return (
     <>
-
       <Container fluid>
         <Row>
           <Col md="12">
@@ -150,33 +171,65 @@ function AllProject() {
             {filterProjectList.map((val, index) => {
               return (
                 <Card
-                key={index}
-                className={`card-with-border-${
-                  val.project_phase === "ร่างคำขออนุมัติ" ? "draft" :
-                  val.project_phase === "ดำเนินการขออนุมัติ" ? "progress" :
-                  val.project_phase === "โครงการอนุมัติ" ? "approved" :
-                  val.project_phase === "เงินโครงการอนุมัติ" ? "approved-budget" :
-                  val.project_phase === "ร่างสรุปผลโครงการ" ? "summary-draft" :
-                  val.project_phase === "ดำเนินการสรุปผล" ? "summary-progress" :
-                  val.project_phase === "ปิดโครงการ" ? "closed" : ""
-                }`}
-                style={{ margin: "0% 0" }}
-              >
+                  key={index}
+                  className={`card-with-border-${
+                    val.project_phase === "ร่างคำขออนุมัติ"
+                      ? "draft"
+                      : val.project_phase === "ดำเนินการขออนุมัติ"
+                      ? "progress"
+                      : val.project_phase === "รออนุมัติโครงการ"
+                      ? "wait-progress"
+                      : val.project_phase === "โครงการอนุมัติ"
+                      ? "approved"
+                      : val.project_phase === "รอเงินโครงการอนุมัติ"
+                      ? "wait-approved-budget"
+                      : val.project_phase === "เงินโครงการอนุมัติ"
+                      ? "approved-budget"
+                      : val.project_phase === "รอสรุปผลโครงการ"
+                      ? "wait-summary-draft"
+                      : val.project_phase === "ร่างสรุปผลโครงการ"
+                      ? "summary-draft"
+                      : val.project_phase === "ดำเนินการสรุปผล"
+                      ? "summary-progress"
+                      : val.project_phase === "ปิดโครงการ"
+                      ? "closed"
+                      : ""
+                  }`}
+                  style={{ margin: "0% 0" }}
+                >
                   <Card.Body>
                     <div className="status-tag">
                       <span
-                          className={`badge badge-${val.project_phase === "ร่างคำขออนุมัติ" ? "draft" :
-                            val.project_phase === "ดำเนินการขออนุมัติ" ? "progress" :
-                            val.project_phase === "โครงการอนุมัติ" ? "approved" :
-                            val.project_phase === "เงินโครงการอนุมัติ" ? "approved-budget" :
-                            val.project_phase === "ร่างสรุปผลโครงการ" ? "summary-draft" :
-                            val.project_phase === "ดำเนินการสรุปผล" ? "summary-progress" :
-                            val.project_phase === "ปิดโครงการ" ? "closed" : ""
-                          }`}
-                          style={{ marginRight: "1%", backgroundColor: stepColors[val.project_phase] }}
-                        >
-                          {val.project_phase}
-                        </span>
+                        className={`badge badge-${
+                          val.project_phase === "ร่างคำขออนุมัติ"
+                            ? "draft"
+                            : val.project_phase === "ดำเนินการขออนุมัติ"
+                            ? "progress"
+                            : val.project_phase === "รออนุมัติ"
+                            ? "wait-progress"
+                            : val.project_phase === "โครงการอนุมัติ"
+                            ? "approved"
+                            : val.project_phase === "รอเงินโครงการอนุมัติ"
+                            ? "wait-approved-budget"
+                            : val.project_phase === "เงินโครงการอนุมัติ"
+                            ? "approved-budget"
+                            : val.project_phase === "รอสรุปผลโครงการ"
+                            ? "wait-summary-draft"
+                            : val.project_phase === "ร่างสรุปผลโครงการ"
+                            ? "summary-draft"
+                            : val.project_phase === "ดำเนินการสรุปผล"
+                            ? "summary-progress"
+                            : val.project_phase === "ปิดโครงการ"
+                            ? "closed"
+                            : ""
+                        }`}
+                        style={{
+                          marginRight: "1%",
+                          backgroundColor: stepColors[val.project_phase],
+                        }}
+                      >
+                        {val.project_phase}
+                      </span>
                       <span
                         className={`badge badge-"warning"`}
                         // className={`badge badge-"warning"`} ดำขาว
@@ -184,44 +237,61 @@ function AllProject() {
                         {val.responsible_agency}
                       </span>
                     </div>
-                    <Card.Title as="h4">
-                      {val.project_name}id {val.id} 
-                    </Card.Title>
-                    <p className="card-category">{val.project_number}</p>
-                    <div
-                      className="d-flex justify-content-between align-items-center"
-                      style={{ backgroundColor: "#f0f0f0" }}
-                    >
-                      <div>
-                        <Button onClick={() => handleShowDetail(val.id)}>
-                          Show Detail
-                        </Button>
-                        {/* You can place additional elements here */}
-                      </div>
-                      <div>
-                        <Button
-                          onClick={() =>
-                            handleDeleteProject(val.id, val.project_name)
-                          }
+
+                    <div className="d-flex flex-column">
+                      <div className="d-flex justify-content-between align-items-center">
+                        <div
+                          className="align-items-center"
+                          style={{ fontSize: "18px", fontWeight: "bold" }}
                         >
-                          ลบ
-                        </Button>
-                        {/* This is the button you want on the right side */}
+                          {val.project_name}id {val.id}
+                          <p className="card-category">{val.project_number}</p>
+                        </div>
+
+                        <div className="d-flex justify-content-end">
+                          <div className="d-inline-flex align-items-end">
+                            <Button
+                              className="btn-details"
+                              variant="warning"
+                              onClick={() => handleShowDetail(val.id)}
+                            >
+                              แสดงรายละเอียด
+                            </Button>
+                            <Button
+                              style={{ marginLeft: "5px" }}
+                              className="btn-decrease"
+                              variant="danger"
+                              onClick={() =>
+                                handleDeleteProject(val.id, val.project_name)
+                              }
+                            >
+                              ลบ
+                            </Button>
+                          </div>
+                        </div>
                       </div>
                     </div>
+
                     <div className="stats">
                       <i className="fas fa-history"></i>
-                      <span>สร้างเมื่อ {formatDate(val.created_at)}</span>
-                      <i className="fas fa-history"></i>
+                      <span> สร้าง {formatDate(val.created_at)} </span>
 
+                      <i
+                        className="fas fa-history"
+                        style={{ marginLeft: "10px" }}
+                      ></i>
                       <span>
                         {val.updated_at === null
-                          ? "update เมื่อ ----"
-                          : `update เมื่อ ${formatDate(val.updated_at)}`}
+                          ? " อัพเดท ----"
+                          : ` อัพเดท ${formatDate(val.updated_at)}`}
                       </span>
                       <span> </span>
-                      <i className="fas fa-pencil-alt"></i>
-                      {val.id_student}
+
+                      <i
+                        className="fas fa-pencil-alt"
+                        style={{ marginLeft: "10px" }}
+                      ></i>
+                      <span> {val.id_student}</span>
                     </div>
                   </Card.Body>
                 </Card>
