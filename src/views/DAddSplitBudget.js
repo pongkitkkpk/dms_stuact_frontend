d1485b8dc45fba98bc08d242953b42e33955da32
Nwpatt <nawapat_link@hotmail.com>
2024-05-13 23:38:38 +0700

    new update / link
    wait for edit Cardfooter and CardBody. Have something not understand about this.

--- src/views/DAddSplitBudget.js
+++ src/views/DAddSplitBudget.js
@@ -16,7 +16,6 @@ function DAddSplitBudget() {
   const [clubName, setClubname] = useState("");
   const [net_budget, setNetBudget] = useState("");
 
-
   const createNetProject = () => {
     Axios.post(`http://localhost:3001/admin/createNetProject`, {
       project_name: project_name,
@@ -50,218 +49,227 @@ function DAddSplitBudget() {
           <div
             style={{
               marginRight: "auto",
-              marginBottom: "10px",
+              marginBottom: "-1px",
+              marginTop: "3px",
               fontSize: "16px",
               color: "white",
             }}
           >
-            งบประมาณโครงการ
+            เพิ่มงบประมาณโครงการ
           </div>
         </CardHeader>
-        <CardBody>
-          <Table striped>
+        <CardBody
+          style={{
+            backgroundColor: "white",
+            paddingBottom: "0px",
+            marginBottom: "0px",
+          }}
+        >
+          <Table striped="columns">
             <tbody>
               <tr style={{ backgroundColor: "white" }}>
-                <td className="head-side-td" style={{ verticalAlign: "top" }}>
-                  <div>งบประมาณโครงการ</div>
-                </td>
-                <td className="back-side-td">
-                  <Table striped="columns">
-                    <thead style={{ backgroundColor: "rgba(255, 139, 19, 0)" }}>
-                      <tr style={{ backgroundColor: "rgba(255, 139, 19, 1)" }}>
-                        <th
-                          style={{
-                            width: "30%",
-                            color: "white",
-                            fontWeight: "bold",
-                          }}
-                        >
-                          ชื่อโครงการ
-                        </th>
-
-                        <th
-                          style={{
-                            width: "15%",
-                            color: "white",
-                            fontWeight: "bold",
-                          }}
-                        >
-                          วิทยาเขต
-                        </th>
-
-                        <th
-                          style={{
-                            width: "20%",
-                            color: "white",
-                            fontWeight: "bold",
-                          }}
-                        >
-                          หน่วยงาน
-                        </th>
-
-                        <th
-                          style={{
-                            width: "11%",
-                            color: "white",
-                            fontWeight: "bold",
-                          }}
-                        >
-                          ปีการศึกษา
-                        </th>
-
-                        <th
-                          style={{
-                            width: "16%",
-                            color: "white",
-                            fontWeight: "bold",
-                          }}
-                        >
-                          งบประมาณสุทธิ
-                        </th>
-                        <th
-                          style={{
-                            width: "10%",
-                            color: "white",
-                            fontWeight: "bold",
-                          }}
-                        ></th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr style={{ backgroundColor: "white" }}>
-                        <td>
-                          <Form.Control
-                            className="font-form-control"
-                            size="sm"
-                            type="text"
-                            placeholder={`ชื่อโครงการ`}
-                            onChange={(event) => {
-                              setProjectName(event.target.value);
-                            }}
-                          />
-                        </td>
-
-                        <td>
-                          <select
-                            onChange={(event) => {
-                              setCampus(event.target.value); // Update the selected campus
-                            }}
-                            className="form-select"
-                            style={{ width: "100%" }}
-                          >
-                            <option value="Bangkok">
-                              <div>กรุงเทพ</div>
-                            </option>
-                            <option value="Prachin">
-                              <div>ปราจีนบุรี</div>
-                            </option>
-                            <option value="Rayong">
-                              {" "}
-                              <div>ระยอง</div>
-                            </option>
-                            {/* Add more options as needed */}
-                          </select>
-                        </td>
-
-                        <td>
-                          <select
-                            onChange={(event) => {
-                              setCodedivision("D04");
-                              setCodeagency(event.target.value);
-
-                              const selectedText =
-                                event.target.options[event.target.selectedIndex]
-                                  .text;
-                              setClubname(selectedText);
-                            }}
-                            required
-                            className="form-select"
-                            style={{ width: "100%" }}
-                          >
-                            <option value="">
-                              <div>กรุณาเลือก ชมรม/หน่วยงาน/องค์กร</div>
-                            </option>
-
-                            {setCode.Divison.D04.Agency.map(
-                              (agencyGroup, index) => {
-                                const campusData = agencyGroup[campus]; // Get data for the selected campus
-                                return (
-                                  campusData && (
-                                    <optgroup
-                                      key={index}
-                                      label={agencyGroup.name}
-                                    >
-  
-                                      {Object.keys(campusData).map(
-                                        (agencyKey) =>
-                                          agencyKey !== "name" && (
-                                            <option
-                                              key={agencyKey}
-                                              value={agencyKey}
-                                            >
-                                              {` ${campusData[agencyKey]}`}
-                                            </option>
-                                          )
-                                      )}
-                                    </optgroup>
-                                  )
-                                );
-                              }
-                            )}
-                          </select>
-                        </td>
-
-                        <td>
-                          <Form.Control
-                            className="font-form-control"
-                            size="sm"
-                            type="text"
-                            placeholder={`พ.ศ.`}
-                            value={yearly}
-                            onChange={(event) => {
-                              setYearly(event.target.value);
-                            }}
-                          />
-                        </td>
-                        <td>
-                          <Form.Control
-                            className="font-form-control"
-                            size="sm"
-                            type="text"
-                            placeholder={`งบประมาณสุทธิ`}
-                            value={net_budget}      
-                            onChange={(event) => {
-                              const value = Number(
-                                event.target.value.replace(/,/g, "")
-                              );
-                              setNetBudget(value.toLocaleString("en-US"));
-                            }}
-                          />
-                        </td>
-
-                        <td>
-                          <div>บาท</div>
-                        </td>
-                      </tr>
-
-                      <tr
+                <Table striped="columns">
+                  <thead style={{ backgroundColor: "rgba(255, 139, 19, 0)" }}>
+                    <tr style={{ backgroundColor: "rgba(255, 139, 19, 1)" }}>
+                      <th
                         style={{
-                          backgroundColor: "rgba(255, 139, 19, .3)",
+                          width: "35%",
+                          color: "white",
+                          // fontWeight: "bold",
                         }}
-                      ></tr>
-                    </tbody>
-                  </Table>
-                </td>
+                      >
+                        ชื่อโครงการ
+                      </th>
+
+                      <th
+                        style={{
+                          width: "15%",
+                          color: "white",
+                          // fontWeight: "bold",
+                        }}
+                      >
+                        วิทยาเขต
+                      </th>
+
+                      <th
+                        style={{
+                          width: "25%",
+                          color: "white",
+                          // fontWeight: "bold",
+                        }}
+                      >
+                        หน่วยงาน
+                      </th>
+
+                      <th
+                        style={{
+                          width: "11%",
+                          color: "white",
+                          // fontWeight: "bold",
+                        }}
+                      >
+                        ปีการศึกษา
+                      </th>
+
+                      <th
+                        style={{
+                          width: "16%",
+                          color: "white",
+                          // fontWeight: "bold",
+                        }}
+                      >
+                        งบประมาณสุทธิ
+                      </th>
+                      <th
+                        style={{
+                          width: "10%",
+                          color: "white",
+                          // fontWeight: "bold",
+                        }}
+                      ></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr style={{ backgroundColor: "white" }}>
+                      <td>
+                        <Form.Control
+                          className="font-form-control"
+                          size="sm"
+                          type="text"
+                          placeholder={`ชื่อโครงการ`}
+                          onChange={(event) => {
+                            setProjectName(event.target.value);
+                          }}
+                        />
+                      </td>
+
+                      <td>
+                        <select
+                          onChange={(event) => {
+                            setCampus(event.target.value); // Update the selected campus
+                          }}
+                          className="form-select font-form-control"
+                          style={{ width: "100%", height: "39px" }}
+                        >
+                          <option value="">
+                            <div>กรุณาเลือก วิทยาเขต</div>
+                          </option>
+                          <option value="Bangkok">
+                            <div>กรุงเทพ</div>
+                          </option>
+                          <option value="Prachin">
+                            <div>ปราจีนบุรี</div>
+                          </option>
+                          <option value="Rayong">
+                            {" "}
+                            <div>ระยอง</div>
+                          </option>
+                          {/* Add more options as needed */}
+                        </select>
+                      </td>
+
+                      <td>
+                        <select
+                          onChange={(event) => {
+                            setCodedivision("D04");
+                            setCodeagency(event.target.value);
+
+                            const selectedText =
+                              event.target.options[event.target.selectedIndex]
+                                .text;
+                            setClubname(selectedText);
+                          }}
+                          required
+                          className="form-select font-form-control"
+                          style={{ width: "100%", height: "39px" }}
+                        >
+                          <option value="">
+                            <div>กรุณาเลือก หน่วยงาน</div>
+                          </option>
+
+                          {setCode.Divison.D04.Agency.map(
+                            (agencyGroup, index) => {
+                              const campusData = agencyGroup[campus]; // Get data for the selected campus
+                              return (
+                                campusData && (
+                                  <optgroup
+                                    key={index}
+                                    label={agencyGroup.name}
+                                  >
+                                    {Object.keys(campusData).map(
+                                      (agencyKey) =>
+                                        agencyKey !== "name" && (
+                                          <option
+                                            key={agencyKey}
+                                            value={agencyKey}
+                                          >
+                                            {` ${campusData[agencyKey]}`}
+                                          </option>
+                                        )
+                                    )}
+                                  </optgroup>
+                                )
+                              );
+                            }
+                          )}
+                        </select>
+                      </td>
+
+                      <td>
+                        <Form.Control
+                          className="font-form-control"
+                          size="sm"
+                          type="text"
+                          placeholder={`พ.ศ.`}
+                          value={yearly}
+                          onChange={(event) => {
+                            setYearly(event.target.value);
+                          }}
+                        />
+                      </td>
+                      <td>
+                        <Form.Control
+                          className="font-form-control"
+                          size="sm"
+                          type="text"
+                          placeholder={`งบประมาณสุทธิ`}
+                          value={net_budget}
+                          onChange={(event) => {
+                            const value = Number(
+                              event.target.value.replace(/,/g, "")
+                            );
+                            setNetBudget(value.toLocaleString("en-US"));
+                          }}
+                        />
+                      </td>
+
+                      <td>
+                        <div>บาท</div>
+                      </td>
+                    </tr>
+
+                    <tr
+                      style={{
+                        backgroundColor: "rgba(255, 139, 19, .3)",
+                      }}
+                    ></tr>
+                  </tbody>
+                </Table>
               </tr>
             </tbody>
           </Table>
         </CardBody>
+
         <CardFooter
           style={{
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
             marginBottom: "10px",
+            marginTop: "0px",
+            paddingTop: "0px",
+            border: "none",
+            backgroundColor: "white",
           }}
         >
           <Button
